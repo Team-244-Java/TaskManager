901e1981bab38a163851c701af6fc3bbeed76805
bentechs1 <ben.techs101@gmail.com>
2023-10-28 21:29:52 -0500

    Mailer

--- src/main/java/edu/cs244/taskpulse/gui/Main.java
+++ src/main/java/edu/cs244/taskpulse/gui/Main.java
@@ -1,23 +1,10 @@
 package edu.cs244.taskpulse.gui;
 
-import edu.cs244.taskpulse.loader.LoginLoader;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import edu.cs244.taskpulse.utils.Mail;
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		//LoginScreen loginScreen = new LoginScreen();
-		// Profile Settings Window GUI
-		//ProfileSettingsLoader profileSettings = new ProfileSettingsLoader();
-		
-		//Registration registration = new Registration();
-		
-		LoginLoader login = new LoginLoader();
-		login.start(primaryStage);
-	}
-
-	public static void main(String[] args) {
-		launch(args);
+public class Main
+{
+	public static void main(String[] args) throws Exception {
+		Mail.sendMail("istevens2@student.ccc.edu");
 	}
 }
